c98665b1b702ba6c8668d0e65ccbaee2c1deb4f1
Dbgamerstarz <impureeuchar@gmail.com>
2019-06-29 19:48:58 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, Response
+
+# emulated camera
+
+
+# Raspberry Pi camera module (requires picamera package)
+# from camera_pi import Camera
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def gen():
+    """Video streaming generator function."""
+    import pyscreenshot as ps
+    while True:
+        # desktop view
+        im=ps.grab()
+        im.save("sc.jpg")
+        frame=open("sc.jpg","rb").read()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, threaded=True)
